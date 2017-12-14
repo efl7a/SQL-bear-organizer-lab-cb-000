c37c7280423a3e25788b8a86f29fe0cbf0c42178
Heather Christman <efl7a@yahoo.de>
2017-12-14 18:18:49 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, alive) VALUES ("Mr. Chocolate", 8, "M", 2);
+INSERT INTO bears (name, age, gender, alive) VALUES ("Rowdy", 4, "M", 1);
+INSERT INTO bears (name, age, gender, alive) VALUES ("Tabitha", 2, "F", 1);
+INSERT INTO bears (name, age, gender, alive) VALUES ("Sergeant Brown", 7, "F", 2);
+INSERT INTO bears (name, age, gender, alive) VALUES ("Melissa", 12, "F", 1);
+INSERT INTO bears (name, age, gender, alive) VALUES ("Grinch", 15, "M", 1);
+INSERT INTO bears (name, age, gender, alive) VALUES ("Wendy", 6, "F", 1);
+INSERT INTO bears (temperament, alive) VALUES ("mean", 2);
